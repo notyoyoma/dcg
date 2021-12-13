1ef8b515a7d00e51ae4be74f1dd9accad675aec6
Marty Naselli <marty@swiftbunny.com>
2021-12-13 14:14:22 -0500

    remove game import from party.test.js

--- src/game/party.test.js
+++ src/game/party.test.js
@@ -1,14 +1,10 @@
-import game from "@/game";
-
-const party = game.party;
-
 test("teleport", () => {
   const pos1 = { x: 1, y: 1, z: 0 };
-  party.teleport(pos1);
-  expect(party.data.location).toMatchObject(pos1);
+  game.party.teleport(pos1);
+  expect(game.party.data.location).toMatchObject(pos1);
 });
 
 test("move", () => {
-  party.moveForward();
-  expect(party.data.location).toMatchObject({ x: 1, y: 2, z: 0 });
+  game.party.moveForward();
+  expect(game.party.data.location).toMatchObject({ x: 1, y: 2, z: 0 });
 });
