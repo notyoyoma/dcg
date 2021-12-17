89350de67defd1def2434ad4f6670e02a832fb9d
Marty Naselli <marty@swiftbunny.com>
2021-12-17 17:16:07 -0500

    fix vue config for prod build

--- vue.config.js
+++ vue.config.js
@@ -13,38 +13,44 @@ module.exports = {
       },
     },
   },
-  configureWebpack: {
-    entry: isDevEnv ? "./src/devMain.js" : "./src/main.js",
-    devServer: {
-      port: 3333,
-      before: (app) => {
-        app.use(bodyParser.json());
+  configureWebpack: (config) => {
+    const terserOptions =
+      config.optimization.minimizer[0].options.terserOptions;
+    terserOptions.mangle.keep_classnames = true;
+    terserOptions.mangle.keep_fnames = true;
+    return {
+      entry: isDevEnv ? "./src/devMain.js" : "./src/main.js",
+      devServer: {
+        port: 3333,
+        before: (app) => {
+          app.use(bodyParser.json());
 
-        // handle READ from data
-        app.get("/data/*", (req, res) => {
-          const filename = path.basename(req.url);
-          fs.readFile(`./data/${filename}`, (_, json) => {
-            let obj = JSON.parse(json);
-            res.json(obj);
+          // handle READ from data
+          app.get("/data/*", (req, res) => {
+            const filename = path.basename(req.url);
+            fs.readFile(`./data/${filename}`, (_, json) => {
+              let obj = JSON.parse(json);
+              res.json(obj);
+            });
           });
-        });
 
-        // handle WRITE to data
-        app.post("/data/*", (req, res, next) => {
-          const filename = path.basename(req.url);
-          fs.writeFile(
-            `./data/${filename}`,
-            JSON.stringify(req.body, null, 2),
-            "utf8",
-            (err) => {
-              if (err) throw err;
-              console.log(`${filename} has been saved!`);
-              res.json({ success: true });
-              next();
-            }
-          );
-        });
+          // handle WRITE to data
+          app.post("/data/*", (req, res, next) => {
+            const filename = path.basename(req.url);
+            fs.writeFile(
+              `./data/${filename}`,
+              JSON.stringify(req.body, null, 2),
+              "utf8",
+              (err) => {
+                if (err) throw err;
+                console.log(`${filename} has been saved!`);
+                res.json({ success: true });
+                next();
+              }
+            );
+          });
+        },
       },
-    },
+    };
   },
 };
